7cc9d850afc8da4aa777691b3405bae834fda428
Ramya <ramyamona16@Ramyas-MacBook-Air.local>
2026-04-20 22:37:24 +0530

    help

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Minikube,  Things are going good from my end--i am your updated version 🚀\n');
+  res.end('Hello from Minikube,  Things are going good from my end--i am your updated version...checking further for changes 🚀\n');
 });
 
 server.listen(port, hostname, () => {
